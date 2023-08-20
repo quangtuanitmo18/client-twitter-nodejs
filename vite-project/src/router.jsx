ba59ba8e47a79ba326911ec8997a6fbfa5842149
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-08-20 13:05:18 +0300

    add page test

--- vite-project/src/router.jsx
+++ vite-project/src/router.jsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Login from "./Login";
-import Home from "./Home";
 import VerifyForgotPasswordToken from "./VerifyForgotPasswordToken";
 import ResetPassword from "./ResetPassword";
 import VerifyEmail from "./verifyEmail";
+import Home from "./home";
+import Test from "./Test";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
   },
   {
     path: "/login/oauth",
