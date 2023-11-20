bc210fdc8d19bf899da4a960ff4a380c1e4e2965
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-11-20 16:16:17 +0300

    css message

--- vite-project/src/Chat.jsx
+++ vite-project/src/Chat.jsx
@@ -12,32 +12,53 @@ export default function Chat() {
     socket.connect();
     socket.on("receive private message", (data) => {
       const content = data.content;
-      setMessages((messages) => [...messages, content]);
+      setMessages((messages) => [
+        ...messages,
+        {
+          content,
+          isSender: false,
+        },
+      ]);
     });
     return () => {
       socket.disconnect();
     };
   }, []);
 
-  const handleSubmit = (e) => {
+  const Send = (e) => {
     e.preventDefault();
     setValue("");
     socket.emit("private message", {
       content: value,
       to: "64b283e8157e8e3f2b07860d", // user_id của client 2
     });
+    setMessages((messages) => [
+      ...messages,
+      {
+        content: value,
+        isSender: true,
+      },
+    ]);
   };
   return (
     <div>
       <h1>Chat</h1>
-      <div>
+      <div className="chat">
         {messages.map((message, index) => (
           <div key={index}>
-            <div>{message}</div>
+            <div className="message-container">
+              <div
+                className={
+                  "message " + (message.isSender ? "message-right" : "")
+                }
+              >
+                {message.content}
+              </div>
+            </div>
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={Send}>
         <input
           type="text"
           onChange={(e) => setValue(e.target.value)}
